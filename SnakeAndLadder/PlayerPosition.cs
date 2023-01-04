1c7b63f1312b9776b471aa3957a217e465d6bad2
Nepolean23 <nlean1996@gmail.com>
2023-01-04 14:10:21 +0530

    [Nepolean]-Player at starting position

---
+++ SnakeAndLadder/PlayerPosition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class PlayerPosition
+    {
+        public static void position()
+        {
+            //Variables
+            int player1;
+            int position = 0;
+
+            Console.WriteLine("The Player Position is :" + position);
+        }
+    }
+}
--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿using SnakeAndLadder;
+
+PlayerPosition.position();
